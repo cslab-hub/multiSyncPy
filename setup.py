92dde31630505875eeb4bc284295612fa9fe00a0
Dan <49373281+HleinaD@users.noreply.github.com>
2022-08-03 16:33:33 +0200

    update version number

--- setup.py
+++ setup.py
@@ -10,7 +10,7 @@ README = (HERE / "README.md").read_text()
 # This call to setup() does all the work
 setup(
     name="multiSyncPy",
-    version="0.0.5",
+    version="0.1.0",
     description="Functions to quantify multivariate synchrony",
     long_description=README,
     long_description_content_type="text/markdown",
