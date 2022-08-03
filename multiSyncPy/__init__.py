a9597a3fa62c00e456b68670430bc12272b2cda2
Dan <49373281+HleinaD@users.noreply.github.com>
2022-08-03 17:34:23 +0200

    Update __init__.py

--- multiSyncPy/__init__.py
+++ multiSyncPy/__init__.py
@@ -4,6 +4,7 @@ This module provides functions to compute multivariate synchrony metrics and to 
 
  * synchrony_metrics - Provides functions used to compute synchrony metrics on multivariate time series.
  * data_generation - Provides functions used to generate synthetic data for the purposes of testing and exploring multivariate synchrony metrics. 
+ * visualisations - Provides functions to calculate and then visualise multivariate synchrony metrics. 
 """
 
 __all__ = ["data_generation", "synchrony_metrics", "visualisations"]
